de94058b2f54172b152214f5b67f1279c69689d3
Frost Monster <frostmonster@yandex.ru>
2022-05-15 21:34:45 +0300

    logging.sh reworked
    Now it checks the initialization and does not require a separate call to the start and initialization functions

--- scripts/logging.sh
+++ scripts/logging.sh
@@ -4,8 +4,8 @@
 
 sn=1 # String counter
 
-# Log function
-log () {
+# Write a Log string function; reads 1) Message to log, 2) Message type: Error(er)/Critical(cr)/Line break (n)/Custom
+logwrite () {
     if [ -d .log/ ]; then
         NST=""
         MSGTYPE="$2: "
@@ -13,22 +13,31 @@ log () {
             "er") MSGTYPE="ERROR: ";;
             "cr") MSGTYPE="CRITICAL: ";;
             "n") NST="\n"; MSGTYPE="";;
-            "") MSGTYPE="";;
+            "") MSGTYPE=""
         esac
     echo -e "$NST $(date +"%Y:%j:%H:%M:%S") $sn:   $MSGTYPE$1" >> "$logfile"
     sn=$((sn+1))
     fi
 }
 
-# Log initialization
+# Logging initialization
 loginit () {
     if ! [ -d .log/ ]; then mkdir .log/; fi
     logfile=".log/$(date +"%Y.%j.%H.%M.%S")${0//'./'/_}.log"
-    log "Logfile have been initialized"
+    logwrite "Logfile have been initialized"
 }
 
-# Script logging start
+# Script logging start; read name of script file
 logstart () {
-    loginit
-    log "${0//'./'/_} started" "n"
+    LOGSTART="$0 STARTED"
+    if ! [ -f logfile ]; then loginit; fi
+    logwrite "${0//'./'/_} started" "n"
+}
+
+# General log script; input is 1) Log Message 2) Message type: Error(er)/Critical(cr)/Line break (n)/Custom
+log () {
+    if $LOGSTATUS; then
+        if ! [ "${LOGSTART//' STARTED'/''}" ]; then logstart; fi
+        logwrite "$1" "n"
+    fi
 }
