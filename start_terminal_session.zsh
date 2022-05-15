3714c72f496c520d1cc05cf9909c08344a875e49
Frost Monster <frostmonster@yandex.ru>
2022-05-15 17:26:15 +0300

    Minor changes

--- start_terminal_session.zsh
+++ start_terminal_session.zsh
@@ -8,7 +8,7 @@ if [[ -z "$TERM_PROGRAM" ]] {
 }
 printf "\n"
 if [[ "$USER" == "root" ]] {
-   USERMSG="\e[30;41m☠ $USER:u TERMINAL SESSION INITIALIZED \e[0m\n"
+   USERMSG="\e[30;41m☠$USER:u TERMINAL SESSION INITIALIZED \e[0m\n"
 } else {
     USERMSG="\e[32m❯ Terminal session initialized at \e[1;32m$USER\e[1;33m@\e[1;32m$HOST\e[0m\n"
 }
