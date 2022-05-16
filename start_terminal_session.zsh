f97527f8ed8b592453766ce26ae705328a5921da
Frost Monster <frostmonster@yandex.ru>
2022-05-17 00:01:12 +0300

    Changed to bash syntax for compatibility

--- start_terminal_session.zsh
+++ start_terminal_session.zsh
@@ -1,15 +1,15 @@
 #!/bin/zsh
 
 # Initializig message
-if ! [ "$TERM_PROGRAM" ] {
+if ! [ "$TERM_PROGRAM" ]; then
     neofetch
-} else {
+else
     printf "\e[32m❯ \e[37;41m \e[0m \e[31m$(lsb_release -sd)\e[0m\n"
-}
+fi
 echo
-if [[ "$USER" == "root" ]] {
+if [[ "$USER" == "root" ]]; then
    USERMSG="\e[30;41m☠ $USER:u TERMINAL SESSION INITIALIZED \e[0m\n"
-} else {
+else
     USERMSG="\e[32m❯ Terminal session initialized at \e[1;32m$USER\e[1;33m@\e[1;32m$HOST\e[0m\n"
-}
+fi
 printf "$USERMSG\n"
